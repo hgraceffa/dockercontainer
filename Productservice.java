56bcf7dfd4a21021de7d648173f43f957d7e07a8
graceffa <graceffa@stud.hs-heilbronn.de>
2020-11-23 06:57:52 +0100

    changed demo

--- Productservice.java
+++ Productservice.java
@@ -1,15 +1,27 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
+/*
+    Simple Class just for demo
+ */
 public class Productservice {
 
-    private static ArrayList<String> productlist = new ArrayList<>();
+    private static ArrayList<String> products = new ArrayList<>();
+    private static ArrayList<String> shoppinglist = new ArrayList<>();
+
 
     public static void main(String[] args) {
-        productlist.add("Apfel");
-        productlist.add("Birne");
-        productlist.add("Banane");
-        productlist.add("Orange");
+        products.add("Apfel");
+        products.add("Birne");
+        products.add("Banane");
+        products.add("Orange");
+        products.add("Pfirsich");
 
-        System.out.println(productlist);
+        for(int i= 0; i < products.size() ; i++) {
+            if(products.get(i).equals("Banane")||products.get(i).equals("Orange")){
+                shoppinglist.add(products.get(i));
+            }
+        }
+        System.out.println(shoppinglist);
     }
 }
